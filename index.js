9806e9f5fa9a9c014ab8ab8280a2df3d16667886
Wicho Flores <lgalindof2200@alumno.ipn.mx>
2025-02-16 19:30:17 -0600

    Web Piloto v0

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Magic!");
+let message;
+message = 'Hello World!';
+console.log(message);
+function changeText() {
+    let heading = document.querySelector('h1');
+    heading.textContent = ':0';
+}
