0a4d468b225e2d304b69fed37f63d90c58b8c333
Wicho Flores <lgalindof2200@alumno.ipn.mx>
2025-02-24 00:28:02 -0600

    Funciones, checar saldo, deposito, retiro, cerrar sesion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.querySelector('.login');
     const accountSection = document.getElementById('account');
     const balanceDisplay = document.getElementById('Balance');
-    let balance = 2.5;
+    let balance = 50;
 
     const validCredentials = {
         username: 'admin',
@@ -23,16 +23,32 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('checkBalance').addEventListener('click', () => {
-            balanceDisplay.textContent = `Saldo: ${balance.toFixed(2)}`;
+            balanceDisplay.textContent = `Saldo: $${balance.toFixed(2)}`;
     });
 
     document.getElementById('deposit').addEventListener('click', () => {
         const amount = parseFloat(prompt('Ingrese la cantidad que desea depositar: '));
         if (!isNaN(amount) && amount > 0) {
             balance += amount;
-            balanceDisplay.textContent = `Saldo: ${balance.toFixed(2)}`;
+            balanceDisplay.textContent = `Saldo: $${balance.toFixed(2)}`;
         } else {
             prompt('Ingrese una cantidad válida');
         }
-    })
+    });
+
+    document.getElementById('withdraw').addEventListener('click', () => {
+        const amount = parseFloat(prompt('Ingrese la cantidad que desea retirar: '));
+        if (!isNaN(amount) && amount > 0 && amount <= balance) {
+            balance -= amount;
+            balanceDisplay.textContent = `Saldo: $${balance.toFixed(2)}`;
+        } else {
+            prompt('Ingrese una cantidad válida');
+        }
+    });
+
+    document.getElementById('logout').addEventListener('click', ()=> {
+       accountSection.style.display = 'none';
+       loginForm.style.display = 'block';
+       balanceDisplay.textContent = '';
+    });
 })
