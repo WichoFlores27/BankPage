2d6ff5d085956d0a947eb5acb82a2c20f7559487
Wicho Flores <lgalindof2200@alumno.ipn.mx>
2025-02-23 23:35:39 -0600

    Update balance display and add deposit functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.querySelector('.login');
     const accountSection = document.getElementById('account');
     const balanceDisplay = document.getElementById('Balance');
-    let balance = 0.0;
+    let balance = 2.5;
 
     const validCredentials = {
         username: 'admin',
@@ -21,4 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Usuario o contraseña incorrectos');
         }
     });
+
+    document.getElementById('checkBalance').addEventListener('click', () => {
+            balanceDisplay.textContent = `Saldo: ${balance.toFixed(2)}`;
+    });
+
+    document.getElementById('deposit').addEventListener('click', () => {
+        const amount = parseFloat(prompt('Ingrese la cantidad que desea depositar: '));
+        if (!isNaN(amount) && amount > 0) {
+            balance += amount;
+            balanceDisplay.textContent = `Saldo: ${balance.toFixed(2)}`;
+        } else {
+            prompt('Ingrese una cantidad válida');
+        }
+    })
 })
